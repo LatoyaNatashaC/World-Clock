2f7634001eca1cbb2b0dffade529bad9897c15a8
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-06-25 18:50:15 -0700

    los angeles ids with date & time are now targeted in java

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("what time is it?");
+let losAngelesElement = document.querySelector("#los-angeles");
+
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+
+losAngelesDateElement.innerHTML = "June 25th 2024";
+
+losAngelesTimeElement.innerHTML = "6:35:15 <small>PM</small>";
