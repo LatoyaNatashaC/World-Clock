72379b4f02b82980d7aa370abe5aabdc7dae9b73
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-06-25 19:04:21 -0700

    date is now displaying

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ let losAngelesDateElement = losAngelesElement.querySelector(".date");
 
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
-losAngelesDateElement.innerHTML = "June 25th 2024";
+losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
 
 losAngelesTimeElement.innerHTML = "6:35:15 <small>PM</small>";
