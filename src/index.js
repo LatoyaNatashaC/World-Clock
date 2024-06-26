3937230c7a29d4afdd11a5090680aab162a77878
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-06-26 10:29:35 -0700

    time & date in new york are now displaying

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format(
   "h:mm:ss [<small>]A[</small>]"
 );
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+let newYorkTime = moment().tz("America/New_York");
+
+newYorkDateElement.innerHTML = moment().format("MMMM Do YYYY");
+newYorkTimeElement.innerHTML = newYorkTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
